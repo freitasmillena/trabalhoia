29d48f6eabc43c1b45b234aea728f3fbf5ecffe8
GuiSSMartins <60627482+GuiSSMartins@users.noreply.github.com>
2022-11-20 16:27:54 +0000

    Terminado 1ª fase trabalho

--- main.py
+++ main.py
@@ -2,36 +2,44 @@ from Mapa import Mapa
 from nodo import Nodo
 from grafo import Grafo
 import sys
+import os
 from ficheiro import Ficheiro
+import pickle
 
 def main():
-   
+
    # ficheiro linhas colunas
    path_ficheiro = sys.argv[1]
-   #print(path_ficheiro)
-   ficheiro = Ficheiro()
-   ficheiro.calculaLC(path_ficheiro)
-   linhas = ficheiro.linhas #int(sys.argv[2])
-   colunas = ficheiro.colunas #30 #int(sys.argv[3])
-   #print(linhas, colunas)
 
-   # A incialização da estrtura Grafo só vai servir para algoritmos de pesquisa NÃO INFORMADA
-   # Isto acontece porque neste tipo de algoritmos, não se considera a velocidade e acelração do veículo
-   '''
-   grafo = Grafo ()
-   grafo.parse("teste.txt", False)
-   grafo.add_heuristica()
-   '''
+   nome_ficheiro = path_ficheiro.split('.')[0]
+   bin_ficheiro = nome_ficheiro + ".bin"
+   isExist = os.path.exists(bin_ficheiro)
 
-   # Mapa só pode ser utilizado para algoritmos   grafo = Grafo ()
-   # grafo_mapa = Grafo ()
-   # grafo_mapa.parse("teste.txt", True)
-   # grafo_mapa.add_heuristica()
+   m = None
 
-   m = Mapa(linhas, colunas)
-   m.parse(path_ficheiro)
-   m.expande_grafo()
-   m.grafo.add_heuristica()
+   if isExist :
+      #print("if")
+      file = open(bin_ficheiro, 'rb')
+      m = pickle.load(file)
+      file.close()
+
+   else:
+      #print("else")
+      #print(path_ficheiro)
+      ficheiro = Ficheiro()
+      ficheiro.calculaLC(path_ficheiro)
+      linhas = ficheiro.linhas #int(sys.argv[2])
+      colunas = ficheiro.colunas #30 #int(sys.argv[3])
+      #print(linhas, colunas)
+
+      m = Mapa(linhas, colunas)
+      m.parse(path_ficheiro)
+      m.expande_grafo()
+      m.grafo.add_heuristica()
+
+      file = open(bin_ficheiro, 'wb')
+      pickle.dump(m, file)
+      file.close()
 
 
    saida = -1
